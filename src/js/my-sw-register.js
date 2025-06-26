8be7753c50b671df7d2458509503f03958fe597f
Titopurwanas11 <tito.purwana.sasmita@gmail.com>
2025-06-26 22:11:01 +0700

    memperbaharui my-sw-register

--- src/js/my-sw-register.js
+++ src/js/my-sw-register.js
@@ -1,11 +1,11 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/storyApps/sw.js')
-            .then(registration => {
-                console.log('ServiceWorker registered');
-            })
-            .catch(err => {
-                console.log('ServiceWorker registration failed: ', err);
-            });
-    });
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/storyApps/sw.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
 }
