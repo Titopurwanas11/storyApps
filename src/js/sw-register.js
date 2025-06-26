933d7b0ce5921d5fef7e7c41191148886cfe1059
Titopurwanas11 <tito.purwana.sasmita@gmail.com>
2025-06-26 12:43:59 +0700

    update fix terakhir

--- src/js/sw-register.js
+++ src/js/sw-register.js
@@ -1,8 +1,9 @@
+// src/js/sw-register.js
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/storyApps/sw.js')
+        navigator.serviceWorker.register('/storyApps/sw.js', { scope: '/storyApps/' }) 
             .then(registration => {
-                console.log('ServiceWorker registered');
+                console.log('ServiceWorker registered with scope: ', registration.scope);
             })
             .catch(err => {
                 console.log('ServiceWorker registration failed: ', err);
